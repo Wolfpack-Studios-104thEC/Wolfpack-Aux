7cd7f92eee5f1d54ef8d18a1a3e387e04ca8fbef
2864Chopper <71991495+2864Chopper@users.noreply.github.com>
2026-02-02 15:30:46 -0500

    Changed second impulse to 800{

--- addons/vehicles/ls_impulsor.hpp
+++ addons/vehicles/ls_impulsor.hpp
@@ -2,7 +2,7 @@ class ls_impulsor_base;
 class WPEC_impulsor_base: ls_impulsor_base {
     speed = 400;
     fuelDrain  = 0;
-    overchargeSpeed = 600;
+    overchargeSpeed = 800;
     overchargeFuelDrain = 0;
     overchargeCooldown = 0;
 };
