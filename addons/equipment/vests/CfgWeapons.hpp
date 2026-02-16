bc2e328a0ed3e8e1e5fb3bf5d4f92f9e9c4f233b
2864Chopper <71991495+2864Chopper@users.noreply.github.com>
2026-02-16 16:24:15 -0500

    Vests finished, have to fix core but it shouldnt change anything.

--- addons/equipment/vests/CfgWeapons.hpp
+++ addons/equipment/vests/CfgWeapons.hpp
@@ -1,1 +1,58 @@
-class CfgWeapons {};
+
+class CfgWeapons
+{
+    class WPS_vests_vest_Arc;
+
+    ARCBASEVESTMACRO(Trooper,A);
+    ARCBASEVESTMACRO(Trooper,B);
+    ARCBASEVESTMACRO(Trooper,C);
+    ARCBASEVESTMACRO(Trooper,D);
+    ARCBASEVESTMACRO(Trooper,E);
+
+    ARCBASEVESTMACRO(EOD,A);
+    ARCBASEVESTMACRO(EOD,B);
+    ARCBASEVESTMACRO(EOD,C);
+    ARCBASEVESTMACRO(EOD,D);
+    ARCBASEVESTMACRO(EOD,E);
+
+    ARCBASEVESTMACRO(Medic,A);
+    ARCBASEVESTMACRO(Medic,B);
+    ARCBASEVESTMACRO(Medic,C);
+    ARCBASEVESTMACRO(Medic,D);
+    ARCBASEVESTMACRO(Medic,E);
+
+    ARCBASEVESTMACRO(TL,A);
+    ARCBASEVESTMACRO(TL,B);
+    ARCBASEVESTMACRO(TL,C);
+    ARCBASEVESTMACRO(TL,D);
+    ARCBASEVESTMACRO(TL,E);
+
+    //Customs
+    ARCCUSTOMVESTMACRO(Graves);
+
+    /*class WPEC_Vest_ARC_Graves: WPEC_Vest_Base
+    {
+        scope = 2;
+        scopecurator = 2;
+        displayName = "[104th] ARC Attachmments (Graves)";
+        model = "\WPEC\WPEC_Vests\ARCVestGraves.p3d";
+        uniformModel = "\WPEC\WPEC_Vests\ARCVestGraves.p3d";
+        hiddenSelections[] = {"Camo1","Camo2"};
+        hiddenSelectionsTextures[] = {"WPEC\WPEC_Vests\data\ARCVestTopGraves_co.paa", "WPEC\WPEC_Vests\data\ARCVestBottomGraves_co.paa"};
+        hiddenSelectionsMaterials[] = {"WPEC\WPEC_Vests\data\ARCVestTop.rvmat", "WPEC\WPEC_Vests\data\ARCVestBottom.rvmat"};
+        class ItemInfo: ItemInfo
+        {
+            uniformModel = "WPEC\WPEC_Vests\ARCVestGraves.p3d";
+            containerClass = "Supply200";
+            class HitpointsProtectionInfo
+            {
+                class Chest
+                {
+                    hitpointName = "HitChest";
+                    armor = 6;
+                    passThrough	= 0.3;
+                };
+            };
+        };
+    };*/
+};
--- addons/equipment/vests/config_lists.hpp
+++ addons/equipment/vests/config_lists.hpp
@@ -1,6 +1,26 @@
 
+#define UNIT_LIST \
+
 
 #define ITEM_LIST \
-
-
-#define UNIT_LIST \
+QGVEST(ARC_Trooper_A_vest), \
+QGVEST(ARC_Trooper_B_vest), \
+QGVEST(ARC_Trooper_C_vest), \
+QGVEST(ARC_Trooper_D_vest), \
+QGVEST(ARC_Trooper_E_vest), \
+QGVEST(ARC_EOD_A_vest), \
+QGVEST(ARC_EOD_B_vest), \
+QGVEST(ARC_EOD_C_vest), \
+QGVEST(ARC_EOD_D_vest), \
+QGVEST(ARC_EOD_E_vest), \
+QGVEST(ARC_Medic_A_vest), \
+QGVEST(ARC_Medic_B_vest), \
+QGVEST(ARC_Medic_C_vest), \
+QGVEST(ARC_Medic_D_vest), \
+QGVEST(ARC_Medic_E_vest), \
+QGVEST(ARC_TL_A_vest), \
+QGVEST(ARC_TL_B_vest), \
+QGVEST(ARC_TL_C_vest), \
+QGVEST(ARC_TL_D_vest), \
+QGVEST(ARC_TL_E_vest), \
+QGVEST(ARC_Graves_vest), \
