0c5400cf49e431215bc6cb884e12ac27939a6ea1
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-03-31 19:42:37 +0300

    Files Restored

---
+++ addons/weapons/grenades/config_lists.hpp
@@ -0,0 +1,34 @@
+#define WEAPONS_LIST \
+
+
+#define AMMO_LIST \
+    "WPEC_SmokeShellBase", \
+    "WPEC_SmokeShellRed", \
+    "WPEC_SmokeShellOrange", \
+    "WPEC_SmokeShellYellow", \
+    "WPEC_SmokeShellBlue", \
+    "WPEC_SmokeShellGreen", \
+    "WPEC_SmokeShellPurple", \
+    "WPEC_ThermalDisruptor_Ammo", \
+    "WPEC_ThermalDisruptorImpact_Ammo", \
+    "WPEC_PersonalShield_Republic_Ammo", \
+    "WPEC_PersonalShieldFull_Republic_Ammo", \
+    "WPEC_SquadShield_Republic_Ammo", \
+    "WPEC_3AS_DCMid_OCPlasmaAmmo"
+
+
+#define MAGAZINE_LIST \
+    "WPEC_3AS_ThermalDetonator_Mag", \
+    "WPEC_3AS_BaridiumCore_Mag", \
+    "WPEC_3AS_SmokeGrenade_White", \
+    "WPEC_3AS_SmokeGrenade_Red", \
+    "WPEC_3AS_SmokeGrenade_Orange", \
+    "WPEC_3AS_SmokeGrenade_Yellow", \
+    "WPEC_3AS_SmokeGrenade_Blue", \
+    "WPEC_3AS_SmokeGrenade_Green", \
+    "WPEC_3AS_SmokeGrenade_Purple", \
+    "WPEC_ThermalDisruptor", \
+    "WPEC_ThermalDisruptorImpact", \
+    "WPEC_PersonalShield_Republic_Mag", \
+    "WPEC_PersonalShieldFull_Republic_Mag", \
+    "WPEC_SquadShield_Republic_Mag"
