ff76cd2db18a8fc92232d0e6ed255e8c979d458d
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-04-11 02:14:35 +0300

    New Repair Crate for Pilots

--- addons/vehicles/landcraft/saber/config.cpp
+++ addons/vehicles/landcraft/saber/config.cpp
@@ -10,7 +10,8 @@ class CfgPatches {
             QGVAR(Saber_BloodHowl),
             QGVAR(Super_Saber),
             QGVAR(Super_Saber_AshHound),
-            QGVAR(Super_Saber_BloodHowl)
+            QGVAR(Super_Saber_BloodHowl),
+			"WPEC_RepairCrate",
         };
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
@@ -73,5 +74,99 @@ class CfgVehicles
 		scope = 2;
 		displayname = "[104th] TX-130 Super (Blood Howl)";
 		hiddenSelectionsTextures[] = {"\Z\wpec\addons\vehicles\landcraft\saber\data\WPEC_Saber_Hull_BloodHowl_co.paa","3AS\3AS_Saber\data\Saber_weapons_co.paa"};
+	};
+	class JLTS_Ammobox_weapons_GAR;
+	class WPEC_RepairCrate: JLTS_Ammobox_weapons_GAR
+	{
+		ace_refuel_fuelCargo=75000;
+		ace_repair_canRepair=1;
+		ACE_maxWeightCarry=10000;
+		ACE_maxWeightDrag=10000;
+		author="104th Eclipse";
+		class SimpleObject
+		{
+			eden=1;
+			animate[]=
+			{
+				
+				{
+					"ammo_hide",
+					0
+				},
+				
+				{
+					"ammoord_hide",
+					1
+				},
+				
+				{
+					"grenades_hide",
+					1
+				},
+				
+				{
+					"support_hide",
+					1
+				}
+			};
+			hide[]={};
+			verticalOffset=0.28400001;
+			verticalOffsetWorld=0;
+			init="''";
+		};
+		editorPreview="\MRC\JLTS\weapons\Crates\data\ui\editorPreviews\JLTS_Ammobox_weapons_GAR.jpg";
+		scope=2;
+		scopeCurator=2;
+		scopeArsenal=2;
+		displayName="Field Repair Crate";
+		icon="iconCrateAmmo";
+		editorCategory=QEGVAR(edcat,aux);
+		editorSubcategory=QEGVAR(edsubcat,crate);
+		maximumLoad=50000;
+		model="\MRC\JLTS\weapons\Crates\crate_1.p3d";
+		class TransportWeapons
+		{
+		};
+		class TransportMagazines
+		{
+		};
+		class TransportBackpacks
+		{
+		};
+		class TransportItems
+		{
+			class _xx_ToolKit
+			{
+				name="ToolKit";
+				count=2;
+			};
+		};
+		class AnimationSources
+		{
+			class Ammo_source
+			{
+				source="user";
+				animPeriod=1;
+				initPhase=1;
+			};
+			class AmmoOrd_source
+			{
+				source="user";
+				animPeriod=1;
+				initPhase=1;
+			};
+			class Grenades_source
+			{
+				source="user";
+				animPeriod=1;
+				initPhase=1;
+			};
+			class Support_source
+			{
+				source="user";
+				animPeriod=1;
+				initPhase=1;
+			};
+		};
 	};
 };
