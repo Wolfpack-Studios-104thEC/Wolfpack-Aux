ef023888e22a687b7ab5c33a7f659c4735a24c6a
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-05-01 22:12:23 +0300

    Custom Vest Added Odin, Juicy

--- addons/customworks/customvests/CfgWeapons.hpp
+++ addons/customworks/customvests/CfgWeapons.hpp
@@ -1,6 +1,7 @@
 class CfgWeapons {
 
     class WPEC_equipment_vests_vest_ARC_Trooper_A;
+    class wps_vests_vest_Cmd1;
     class GVEST(ARC_Graves): WPEC_equipment_vests_vest_ARC_Trooper_A
     {
         scope = 2;
@@ -10,6 +11,28 @@ class CfgWeapons {
         class XtdGearInfo {
             model = QGVAR(Custom_Arc_rank_vests);
             person = "Graves";
+        };
+    };
+    class GVEST(P2_Odin): wps_vests_vest_Cmd1
+    {
+        scope = 2;
+        scopecurator = 2;
+        displayName = "[104th] P2 Vest Attachmments (Odin)";
+        hiddenSelectionsTextures[] = {QPATHTOF(data\command\CMDVestTopOdin_co.paa), QPATHTOF(data\command\CMDVestBottomOdin_co.paa)};
+        class XtdGearInfo {
+            model = QGVAR(Custom_p2_rank_vests);
+            person = "Odin";
+        };
+    };
+    class GVEST(P2_Juicy): wps_vests_vest_Cmd1
+    {
+        scope = 2;
+        scopecurator = 2;
+        displayName = "[104th] P2 Vest Attachmments (Juicy)";
+        hiddenSelectionsTextures[] = {QPATHTOF(data\p2\P2VestTopJuicy_co.paa), QPATHTOF(data\p2\P2VestBottomJuicy_co.paa)};
+        class XtdGearInfo {
+            model = QGVAR(Custom_p2_rank_vests);
+            person = "Juicy";
         };
     };
 };
--- addons/customworks/customvests/config_lists.hpp
+++ addons/customworks/customvests/config_lists.hpp
@@ -1,4 +1,6 @@
 
 
 #define ITEM_LIST \
-QGVEST(ARC_Graves)
+QGVEST(ARC_Graves), \
+QGVEST(P2_Odin), \
+QGVEST(P2_Juicy)
