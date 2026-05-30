c6b2bc22599837c88af0e4d5ba6a367fd3a11ac8
ouranium <ouranium@gmail.com>
2026-05-30 14:47:30 -0500

    Graves' fix

--- addons/customworks/customvests/CfgWeapons.hpp
+++ addons/customworks/customvests/CfgWeapons.hpp
@@ -5,8 +5,8 @@ class CfgWeapons {
     class wps_vests_vest_Kama1;
     class GVEST(ARC_Graves): WPEC_equipment_vests_vest_ARC_Trooper_A
     {
-        scope = 1;
-        scopecurator = 1;
+        scope = 2;
+        scopecurator = 2;
         displayName = "[104th] ARC Attachmments (Graves)";
         hiddenSelectionsTextures[] = {QPATHTOF(data\arc\ARCVestTopGraves_co.paa), QPATHTOF(data\arc\ARCVestBottomGraves_co.paa)};
         class XtdGearInfo {
