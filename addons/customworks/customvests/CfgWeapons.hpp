e24fbdc55796b4944c13be996369fbd03b013994
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-05-30 03:06:44 +0300

    Small changes

--- addons/customworks/customvests/CfgWeapons.hpp
+++ addons/customworks/customvests/CfgWeapons.hpp
@@ -5,8 +5,8 @@ class CfgWeapons {
     class wps_vests_vest_Kama1;
     class GVEST(ARC_Graves): WPEC_equipment_vests_vest_ARC_Trooper_A
     {
-        scope = 2;
-        scopecurator = 2;
+        scope = 1;
+        scopecurator = 1;
         displayName = "[104th] ARC Attachmments (Graves)";
         hiddenSelectionsTextures[] = {QPATHTOF(data\arc\ARCVestTopGraves_co.paa), QPATHTOF(data\arc\ARCVestBottomGraves_co.paa)};
         class XtdGearInfo {
@@ -16,8 +16,8 @@ class CfgWeapons {
     };
     class GVEST(P2_Odin): wps_vests_vest_Cmd1
     {
-        scope = 2;
-        scopecurator = 2;
+        scope = 1;
+        scopecurator = 1;
         displayName = "[104th] P2 Vest Attachmments (Odin)";
         hiddenSelectionsTextures[] = {QPATHTOF(data\command\CMDVestTopOdin_co.paa), QPATHTOF(data\command\CMDVestBottomOdin_co.paa)};
         class XtdGearInfo {
@@ -27,8 +27,8 @@ class CfgWeapons {
     };
     class GVEST(P2_Juicy): wps_vests_vest_Cmd1
     {
-        scope = 2;
-        scopecurator = 2;
+        scope = 1;
+        scopecurator = 1;
         displayName = "[104th] P2 Vest Attachmments (Juicy)";
         hiddenSelectionsTextures[] = {QPATHTOF(data\p2\P2VestTopJuicy_co.paa), QPATHTOF(data\p2\P2VestBottomJuicy_co.paa)};
         class XtdGearInfo {
@@ -38,8 +38,8 @@ class CfgWeapons {
     };
     class GVEST(P2_Queue): wps_vests_vest_Kama1
     {
-        scope = 2;
-        scopecurator = 2;
+        scope = 1;
+        scopecurator = 1;
         displayName = "[104th] P2 Vest Attachmments (Juicy)";
         hiddenSelectionsTextures[] = {QPATHTOF(data\command\CMDVestBottomQueue_co.paa)};
         class XtdGearInfo {
