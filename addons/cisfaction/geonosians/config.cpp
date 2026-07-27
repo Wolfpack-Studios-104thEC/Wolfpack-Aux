6ec3fba2b7f0ac8d5e95bd04c5bddf4f217e36e3
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-07-27 19:48:35 +0300

    Geonosians

---
+++ addons/cisfaction/geonosians/config.cpp
@@ -0,0 +1,30 @@
+class cfgPatches
+{
+	class WPEC_CIS_Geonosian_Addon
+	{
+		name="WPEC_CIS_Geonosis";
+		author="WPEC Dev";
+		requiredVersion=2.2;
+		requiredAddons[]=
+		{
+			"JLTS_Core",
+			"ls_loadorder",
+            "WPEC_CIS"
+		};
+		weapons[]=
+		{
+		};
+		units[]=
+		{
+		};
+	};
+};
+class CfgWeapons
+{
+
+};
+
+class CfgVehicles
+{
+	
+};
