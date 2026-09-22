725a6931aadd86fac090760f71262f01322e2a94
Alexander Stefan <tetsuyahashirama@yahoo.ro>
2026-09-22 22:05:40 +0300

    Killa's new toy on steroids

--- addons/weapons/CfgVehicles.hpp
+++ addons/weapons/CfgVehicles.hpp
@@ -1,4 +1,16 @@
 
 class CfgVehicles
 {
+
+    class ls_vehicle_turretDroid_base;
+    class WPEC_GroundTurret: ls_vehicle_turretDroid_base
+    {
+        scope = 1;
+        author = "Legion Studio Stuff";
+        displayName = "[104th] A Turret? Perry The Turret !";
+        armor = 100;
+        faction="wpec_faction_eclipse";
+
+    };
+
 };
--- addons/weapons/CfgWeapons.hpp
+++ addons/weapons/CfgWeapons.hpp
@@ -1,3 +1,23 @@
 class CfgWeapons
-{
+{   
+    class CBA_MiscItem;
+	class ls_miscItem_base;
+	class ls_vehicle_turretDroid_packed: ls_miscItem_base
+    {
+        class ItemInfo;
+    };
+    class Tureta_Impachetata: ls_vehicle_turretDroid_packed
+    {
+        scope=2;
+        displayName="[104th] Packed Turret";
+        descriptionShort="Fuck Off?";
+        ls_common_deployObject="WPEC_GroundTurret";
+		ls_common_deployStatement="call ls_vehicles_fnc_deployTurretDroid";
+		ls_common_deployTime=1;
+		ls_equipment=1;
+        class ItemInfo: ItemInfo
+		{
+			mass=25;
+		};
+    };
 };
